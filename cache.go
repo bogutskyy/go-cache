dd362f3830ad4fc416543aef4dc87890a730bb67
Yaroslav Bogutsky <yaroslav@bogutsky.name>
2025-03-29 23:39:02 +0200

    Added im memory cache

---
+++ cache.go
@@ -0,0 +1,21 @@
+package cache
+
+type Cache struct {
+	items map[string]interface{}
+}
+
+func New() Cache {
+	return Cache{items: make(map[string]interface{})}
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+	c.items[key] = value
+}
+
+func (c *Cache) Get(key string) interface{} {
+	return c.items[key]
+}
+
+func (c *Cache) Delete(key string) {
+	delete(c.items, key)
+}
